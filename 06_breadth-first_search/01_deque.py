f934adfab571bb6c9aecf4bbab63182b9a9a1d6f
vpalex999 <vpalex@mail.ru>
2021-06-28 09:27:18 +0500

    p.146

---
+++ 06_breadth-first_search/01_deque.py
@@ -0,0 +1,37 @@
+"""
+Поиск в ширину."""
+from collections import deque
+
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+
+graph = {}
+graph['you'] = ['alice', 'bob', 'claire']
+graph['bob'] = ['anuj', 'peggy']
+graph['alice'] = ['peggy']
+graph['claire'] = ['thom', 'jonny']
+graph['anuj'] = []
+graph['peggy'] = []
+graph['thom'] = []
+graph['jonny'] = []
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = set()
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!!!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.add(person)
+    return False
+
+
+search('you')
